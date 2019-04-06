c283e5122d682d9226231aef0dcf9681bf451388
Daniel Salinas <imsplitbit@gmail.com>
2019-04-06 10:16:51 -0500

    rename basedir

--- test_user.py
+++ test_user.py
@@ -1,7 +1,6 @@
 import unittest
-import os
 import json
-from app import create_app, db
+from usrmgr import create_app, db
 
 
 class UserTestCase(unittest.TestCase):
@@ -18,9 +17,16 @@ class UserTestCase(unittest.TestCase):
 
         with self.app.app_context():
             db.create_all()
-    
+
     def tearDown(self):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
-    
+
+    def test_user_creation(self):
+        """Test API can create a user (POST)"""
+        res = self.client().post('/users/', data=self.testuser)
+        self.assertEqual(res.status_code, 201)
+        data = json.loads(res.data)
+        self.assertEqual(self.testuser['first_name'],
+                         data['result']['first_name'])
